59de1da53f79936b1326da331cf2650255c52a6e
worscik <58079969+worscik@users.noreply.github.com>
2023-02-21 12:46:57 +0100

    Create project
    create project

---
+++ src/main/java/pl/naukaslowek/NaukaslowekApplication.java
@@ -0,0 +1,13 @@
+package pl.naukaslowek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukaslowekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukaslowekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/naukaslowek/NaukaslowekApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.naukaslowek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukaslowekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
